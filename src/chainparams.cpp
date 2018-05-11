d0d24a91f577d4c540c3789a6f9a2d694fb7d50d
HarryWu <imharrywu@users.noreply.github.com>
2018-05-11 21:33:24 +0800

    Update chainparams.cpp

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -162,7 +162,8 @@ public:
 
         checkpointData = (CCheckpointData) {
             boost::assign::map_list_of
-            (        0, uint256S("0x0074cb9c4b30aff770f676afd18ddab72f2e7f594e212b5a8460a7634730e10a")),
+            (        0, uint256S("0x0074cb9c4b30aff770f676afd18ddab72f2e7f594e212b5a8460a7634730e10a"))
+            (        1, uint256S("0x00004e9f18d1849fe4f663323cdac91bb306858675f8918d0768229035c19fb6")),
             1523529400, // * UNIX timestamp of last checkpoint block
             1,          // * total number of transactions between genesis and last checkpoint
                         //   (the tx=... number in the SetBestChain debug.log lines)
