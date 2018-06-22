5d393832cccd8deb662e583b68055d256042e67a
Icbitproject <16358164@qq.com>
2018-06-22 11:49:17 +0800

    0622

--- src/main.cpp
+++ src/main.cpp
@@ -101,7 +101,7 @@ static void CheckBlockIndex(const Consensus::Params& consensusParams);
 /** Constant stuff for coinbase transactions we create: */
 CScript COINBASE_FLAGS;
 
-const string strMessageMagic = "Bitcoin Signed Message:\n";
+const string strMessageMagic = "Lxrcoins Signed Message:\n";//<coingo.vip>
 
 // Internal stuff
 namespace {
